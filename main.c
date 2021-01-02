b7f5b8d31fe054d39c875241bf42986f0c663435
elChupaCambra <claudio.cambra@gmail.com>
2021-01-02 22:48:24 +0100

    Created window

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include <gtk/gtk.h>
 
-#include "exampleapp.h"
+static void
+activate (GtkApplication* app, gpointer user_data) {
+  GtkWidget *window;
 
-int main (int argc, char *argv[]) {
-  return g_application_run (G_APPLICATION (example_app_new()), argc, argv);
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
+  gtk_widget_show_all (window);
+}
+
+int
+main (int argc, char **argv) {
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
 }
