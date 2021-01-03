00ae77d2e7f22d0040260df67da3adf4d46de67c
elChupaCambra <claudio.cambra@gmail.com>
2021-01-03 18:18:42 +0100

    Figure out how grids work and added more comments!

--- main.c
+++ main.c
@@ -17,28 +17,38 @@ static void
 activate (GtkApplication* app, gpointer user_data) {
 	// Setting GtkWidget pointer variables
   GtkWidget *window;
-  GtkWidget *button;
-  // 'GtkButtonBox'es are used to control the size and layout of buttons
-  GtkWidget *button_box;
+  GtkWidget *printdice_button;
+  GtkWidget *test_button;
+  GtkWidget *quit_button;
+  GtkWidget *grid;
 
 	// Creates new GtkWindow and stores it in 'window' pointer
   window = gtk_application_window_new (app);
   // Below functions take a GtkWindow* pointer. But we're not casting 'window' with (GtkWindow*).
   // Pointer is cast with GTK_WINDOW macro, which will check if the pointer is an instance of GtkWindow class. 
   gtk_window_set_title (GTK_WINDOW (window), "GTK-Dice");
-  gtk_window_set_default_size (GTK_WINDOW (window), 640, 480);
+  gtk_container_set_border_width (GTK_CONTAINER (window), 10);
   
-  // gtk_button_box_new takes a GtkOrientation enum as a parameter
-  button_box = gtk_button_box_new (GTK_ORIENTATION_HORIZONTAL);
-  // Adds the button_box widget to the window widget
-  gtk_container_add (GTK_CONTAINER (window), button_box);
+  // Grid container that will pack our buttons
+  grid = gtk_grid_new ();
+  gtk_container_add (GTK_CONTAINER (window), grid);
   
   // gtk_button_new_with_label returns a GtkButton
-  button = gtk_button_new_with_label ("Print Dice");
+  printdice_button = gtk_button_new_with_label ("Print Dice");
   // g_signal_connect connects button to function print_die
   // Since print_die does not use any data as input, NULL is passed to it
-  g_signal_connect (button, "clicked", G_CALLBACK (print_die), NULL);
-  gtk_container_add (GTK_CONTAINER (button_box), button);
+  g_signal_connect (printdice_button, "clicked", G_CALLBACK (print_die), NULL);
+  // place printdice_button in grid-cell(0,0), and make it fill just 1 cell horizontally and vertically
+	gtk_grid_attach (GTK_GRID (grid), printdice_button, 0, 0, 1, 1);
+	
+	test_button = gtk_button_new_with_label ("Test button");
+	gtk_grid_attach (GTK_GRID (grid), test_button, 1, 0, 1, 1);
+	
+	quit_button = gtk_button_new_with_label ("Quit");
+	// g_signal_connect_swapped() allows you to specify what the callback function should take as parameter by letting you pass it as data. 
+	// In this case the function being called back is gtk_widget_destroy() and the window pointer is passed to it.
+	g_signal_connect_swapped (quit_button, "clicked", G_CALLBACK (gtk_widget_destroy), window);
+	gtk_grid_attach (GTK_GRID (grid), quit_button, 0, 1, 2, 1);
   
   // Shows window!
   gtk_widget_show_all (window);
